7b34d45c4db54d8628f5e3cb27e292302c06e9e6
jenty <jentino@gmail.com>
2017-12-02 17:07:23 +0200

    sd

--- server.js
+++ server.js
@@ -8,24 +8,9 @@ var pg = require('pg');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
-const DBINDEX = path.join(__dirname, 'db.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
-  .get('/db', function (request, response) {
-      pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
-          done();
-          if (err)
-           { console.error(err); response.send("Error " + err); }
-          else
-           { 
-             console.log(result.rows);
-              response.render(DBINDEX, {results: result.rows} ); 
-          }
-        });
-      });
-    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
     // var pg = require('pg');
