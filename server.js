77b5432dc92475219275620b2fa796b33c6a9051
jenty <jentino@gmail.com>
2017-12-02 15:17:30 +0200

    postgres

--- server.js
+++ server.js
@@ -4,12 +4,30 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
 
+
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  var pg = require('pg');
+  
+  server.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+         { 
+           console.log(result.rows);
+            response.render('pages/db', {results: result.rows} ); 4
+        }
+      });
+    });
+  });
 
 const wss = new SocketServer({ server });
 
